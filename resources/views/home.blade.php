44d57ddd9731b93b15c8decc17bc25c3119c6619
Anthoy Cardona <akcardona0912@uotlook.com>
2021-04-20 21:17:37 -0600

    Se edito home.blade.php y corrio la migraciones con la base de datos remota con mysql

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+                    <div class="card-body">
+                        @if (session('status'))
+                            <div class="alert alert-success" role="alert">
+                                {{ session('status') }}
+                            </div>
+                        @endif
+
+                        <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
+                            <div class="flex justify-center pt-8 sm:justify-start sm:pt-0">
+                                <h1 class="text-gray-200">Examen Final ISW811 - Anthony Keylor Cardona Mairena</h1>
+                            </div>
                         </div>
-                    @endif
-
-                    {{ __('You are logged in!') }}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
